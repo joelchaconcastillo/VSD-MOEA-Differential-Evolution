57cc90864b6fec8af008ab5cce1fc6763c9d4e93
joel.chacon@cimat.mx <joel.chacon@cimat.mx>
2019-09-19 13:42:46 -0500

    ..

--- jMetalCpp-1.7-DE/HomeworkCluster/Generatorexec.pl
+++ jMetalCpp-1.7-DE/HomeworkCluster/Generatorexec.pl
@@ -6,20 +6,22 @@ use Getopt::Long;
 my $file = "ExecutionFileDiversity2";
 my $fout;
 open($fout, '>' ,$file);
-my $Path = "/home/joel.chacon/Chacon/Tesis/jMetalCpp-1.7-DE";
+my $Path = `cd ..; pwd`; #   "/home/joel.chacon/Chacon/DE_diversity/CEC2018/";
+chomp $Path;
+
 my $Sed=0;
 my $NEjecutions=35;
 
 #my @Algorithms= ("MOEAD_main", "MOEAD_main");
 #my @Algorithms= ("NSGAII_main", "NSGAII_main");
-my @Algorithms= ("NSGAII_main","FastSMSEMOA_main");
+my @Algorithms= ("NSGAII_main");
 #my @Algorithms= ("MOEAD_main" ,"NSGAII_main", "GDE3_main");
 #my @Algorithms= ("FastSMSEMOA_main", "MOEAD_main", "NSGAII_main", "GDE3_main");
 #my @Algorithms= ("NSGAII_main", "", "" ,"GDE3_main", "NSGAII_main", "GDE3_main", "NSGAII_main", "GDE3_main" );
 #my @Algorithms= ("SMSEMOA_main"); #, "MOEAD_main");
 #my @DirAlgorithms= ("Run_Gen250mil_Pob100_Dynamic_3Objs/SMSEMOA");#, "Run_Gen250mil_Pob100_Dynamic_3Objs/MOEAD");
 #my @DirAlgorithms= ("Run_Gen250mil_Pob_100_DE_Dynamic_Hybrid/SMSEMOA", "Run_Gen250mil_Pob_100_DE_Dynamic_Hybrid/MOEAD", "Run_Gen250mil_Pob_100_DE_Dynamic_Hybrid/NSGAII", "Run_Gen250mil_Pob_100_DE_Dynamic_Hybrid/GDE3");
-my @DirAlgorithms= ("DE-Gen250th-NObjs/NSGAII" ,"DE-Gen250th-NObjs/SMS-EMOA");
+my @DirAlgorithms= ("NSGAII");
 #my @DirAlgorithms= ("DNR-DE-Gen250th-NObjs/UMOEAD-Obj-2", "DNR-DE-Gen250th-NObjs/UMOEAD-Obj-3", "DNR-DE-Gen250th-NObjs/UMOEAD-Obj-10" );
 #my @DirAlgorithms= ("DNR-DE-Gen250th-NObjs/UMOEAD-Obj-10", "DNR-DE-Gen250th-NObjs/NSGAII-Obj-10", "DNR-DE-Gen250th-NObjs/GDE3-Obj-10");#, "DNR-DE-Gen250th-NObjs/NSGAII-Obj-3", "DNR-DE-Gen250th-NObjs/GDE3-Obj-2", "DNR-DE-Gen250th-NObjs/GDE3-Obj-3" );
 #
@@ -37,7 +39,7 @@ my @NObjectives = ("2");
 #
 #
 @Algorithms= ("NSGAII_main");
-@DirAlgorithms= ("DE-Gen250th-NObjs/NSGAII");
+@DirAlgorithms= ("NSGAII");
 
 for(my $i = 0; $i < scalar(@Algorithms); $i++ )
 {
@@ -96,40 +98,40 @@ for(my $i = 0; $i < scalar(@Algorithms); $i++ )
 }
 #
 ###sms-emoa
-
-@Algorithms= ("FastSMSEMOA_main");
-@DirAlgorithms= ("DE-Gen250th-NObjs/SMS-EMOA");
-@NObjectives = ("2");
-
-@NVariables = ("24", "24", "24", "24", "24", "24", "24", "24", "24","6", "11", "11", "11", "11", "11", "21");
-
-@Instances = ("WFG1","WFG2", "WFG3", "WFG4", "WFG5", "WFG6", "WFG7", "WFG8", "WFG9","DTLZ1","DTLZ2", "DTLZ3", "DTLZ4", "DTLZ5", "DTLZ6", "DTLZ7"); ##La ZDT5 es para cadenas binarias.....
-for(my $i = 0; $i < scalar(@Algorithms); $i++ )
-{
-        my $contv = 0;
-	foreach my $Instance(@Instances) ##Para cada instancia WFG...
-	{
-		for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semillas de cada instancia..
-		{
-			print $fout "$Sed~$Path/bin/main/".$Algorithms[$i]." $Instance Real ".$NVariables[$contv]." ".$NObjectives[$i]." $Sed $Path/".$DirAlgorithms[$i]." \n";
-		}
-	   $contv++;
-	}
-}
-
-
-
- @Instances= ("UF1", "UF2", "UF3", "UF4", "UF5", "UF6", "UF7"); 
-for(my $i = 0; $i < scalar(@Algorithms); $i++ )
-{
-	foreach my $Instance(@Instances) 
-	{
-		for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semillas de cada instancia..
-		{
-
-			print $fout "$Sed~$Path/bin/main/".$Algorithms[$i]." $Instance Real 30 2 $Sed $Path/".$DirAlgorithms[$i]." \n";
-		}
-	}
-
-}
-
+#
+#@Algorithms= ("FastSMSEMOA_main");
+#@DirAlgorithms= ("DE-Gen250th-NObjs/SMS-EMOA");
+#@NObjectives = ("2");
+#
+#@NVariables = ("24", "24", "24", "24", "24", "24", "24", "24", "24","6", "11", "11", "11", "11", "11", "21");
+#
+#@Instances = ("WFG1","WFG2", "WFG3", "WFG4", "WFG5", "WFG6", "WFG7", "WFG8", "WFG9","DTLZ1","DTLZ2", "DTLZ3", "DTLZ4", "DTLZ5", "DTLZ6", "DTLZ7"); ##La ZDT5 es para cadenas binarias.....
+#for(my $i = 0; $i < scalar(@Algorithms); $i++ )
+#{
+#        my $contv = 0;
+#	foreach my $Instance(@Instances) ##Para cada instancia WFG...
+#	{
+#		for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semillas de cada instancia..
+#		{
+#			print $fout "$Sed~$Path/bin/main/".$Algorithms[$i]." $Instance Real ".$NVariables[$contv]." ".$NObjectives[$i]." $Sed $Path/".$DirAlgorithms[$i]." \n";
+#		}
+#	   $contv++;
+#	}
+#}
+#
+#
+#
+# @Instances= ("UF1", "UF2", "UF3", "UF4", "UF5", "UF6", "UF7"); 
+#for(my $i = 0; $i < scalar(@Algorithms); $i++ )
+#{
+#	foreach my $Instance(@Instances) 
+#	{
+#		for(my $Sed = 1; $Sed <=35; $Sed++) ##Realizar 35 ejecuciones con distintas semillas de cada instancia..
+#		{
+#
+#			print $fout "$Sed~$Path/bin/main/".$Algorithms[$i]." $Instance Real 30 2 $Sed $Path/".$DirAlgorithms[$i]." \n";
+#		}
+#	}
+#
+#}
+#
